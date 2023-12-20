74e015568f0ed67c7efbf645f762a1b1b38eec4b
Aditya <aditya.banik@12thwonder.com>
2023-12-20 17:25:57 +0530

    final-commit-pre-release

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,38 @@
 <svelte:options customElement="wix-vertical-tabs-scheduling" />
 
 <script>
+  import { onDestroy, onMount, tick } from "svelte";
   import Accordian from "./lib/Accordian.svelte";
   import AccordianItem from "./lib/AccordianItem.svelte";
 
   import { fade } from "svelte/transition";
+
+  let imageHeight = 0;
+  function updateHeight() {
+    const imgElement = document.querySelector(".image-container img");
+    if (imgElement) {
+      imageHeight = imgElement.clientHeight;
+      document.documentElement.style.setProperty(
+        "--image-height",
+        `${imageHeight}px`
+      );
+    }
+  }
+  onMount(() => {
+    window.addEventListener("resize", updateHeight);
+  });
+
+  onDestroy(() => {
+    window.removeEventListener("resize", updateHeight);
+  });
+
+  $: console.log(imageHeight);
 </script>
 
 <section class="accordian-container">
-  <h1><span>All-in-one</span> verification hub to optimize your product testing process</h1>
+  <h1>
+    <span>All-in-one</span> verification hub to optimize your product testing process
+  </h1>
   <div class="accordian">
     <Accordian>
       <AccordianItem>
@@ -18,11 +42,13 @@
             Trace, record, analyze, and prioritize the requirements to
             successfully fulfil all product testing goals.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_bbc6cbbe424c4c8ebe4ac6b519cbd45d~mv2.png"
-            alt="Resources Availability Status in Real-time"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              on:load={updateHeight}
+              src="https://static.wixstatic.com/media/43794f_bbc6cbbe424c4c8ebe4ac6b519cbd45d~mv2.png"
+              alt="Resources Availability Status in Real-time"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem>
@@ -33,11 +59,12 @@
             defined by engineers during the design phase. You can also easily
             track progress while the test is going on.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_cca6ab06d6a8420aacec8e823436c90d~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_cca6ab06d6a8420aacec8e823436c90d~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem>
@@ -48,11 +75,12 @@
             and reuse the same test cases several times. All you have to do is
             change the testing parameters with different values.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_1e87966fdfbc4982b002bcb988717bab~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_1e87966fdfbc4982b002bcb988717bab~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem>
@@ -64,61 +92,76 @@
             requirements, updates all requirements status in real-time, and
             analyzes the impact of requirement changes.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_cca6ab06d6a8420aacec8e823436c90d~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_cca6ab06d6a8420aacec8e823436c90d~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
     </Accordian>
   </div>
 </section>
 
-
 <section class="accordian-container right-align">
-  <h1>Orchestrate your testing
-    schedule, collaborate with
-    teams, and take advantage of <span>optimum resource allocation</span></h1>
+  <h1>
+    Orchestrate your testing schedule, collaborate with teams, and take
+    advantage of <span>optimum resource allocation</span>
+  </h1>
   <div class="accordian">
     <Accordian>
       <AccordianItem alignRight={true}>
         <svelte:fragment slot="title">Prototype Management</svelte:fragment>
         <article>
           <p>
-            Enable your teams to interact effectively, keep track of resources in real-time and avoid double-booking while minimizing conflicts between teams over testing resources.  
+            Enable your teams to interact effectively, keep track of resources
+            in real-time and avoid double-booking while minimizing conflicts
+            between teams over testing resources.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_7f42670e3f95466eac63adc9f07367e1~mv2.png"
-            alt="Resources Availability Status in Real-time"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_7f42670e3f95466eac63adc9f07367e1~mv2.png"
+              alt="Resources Availability Status in Real-time"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem alignRight={true}>
-        <svelte:fragment slot="title">Integrated Work Order management</svelte:fragment>
+        <svelte:fragment slot="title"
+          >Integrated Work Order management</svelte:fragment
+        >
         <article>
           <p>
-            Standardize and effortlessly map all your business processes to streamline operation with more accountability and zero ambiguity. No matter how stringent or complex your testing environment is, Titan offers a seamless experience, every time! 
+            Standardize and effortlessly map all your business processes to
+            streamline operation with more accountability and zero ambiguity. No
+            matter how stringent or complex your testing environment is, Titan
+            offers a seamless experience, every time!
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/454d4b_52cd0f152dfc424e9bb2295e1f81ad3a~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/454d4b_52cd0f152dfc424e9bb2295e1f81ad3a~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem alignRight={true}>
-        <svelte:fragment slot="title">Assets and Resource Management</svelte:fragment>
+        <svelte:fragment slot="title"
+          >Assets and Resource Management</svelte:fragment
+        >
         <article>
           <p>
-            Enjoy an easy-to-use system to streamline the entire test scheduling process from receiving test requests, to filtering and assigning them to a resource, and finally executing the test. 
+            Enjoy an easy-to-use system to streamline the entire test scheduling
+            process from receiving test requests, to filtering and assigning
+            them to a resource, and finally executing the test.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_9870822e316c44288988df8b201fabed~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_9870822e316c44288988df8b201fabed~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
     </Accordian>
@@ -126,53 +169,68 @@
 </section>
 
 <section class="accordian-container">
-  <h1>Embrace the <span>robust</span> features
-    that make test management
-    a cakewalk for your team</h1>
+  <h1>
+    Embrace the <span>robust</span> features that make test management a cakewalk
+    for your team
+  </h1>
   <div class="accordian">
     <Accordian>
       <AccordianItem>
-        <svelte:fragment slot="title">Team Collaboration and Conflict Management</svelte:fragment>
+        <svelte:fragment slot="title"
+          >Team Collaboration and Conflict Management</svelte:fragment
+        >
         <article>
           <p>
-            Meet the needs of your users by testing your prototype with real consumers and validating your product concept before development. An ideal tool for prototype specifications management and tracking them for every test run. 
+            Meet the needs of your users by testing your prototype with real
+            consumers and validating your product concept before development. An
+            ideal tool for prototype specifications management and tracking them
+            for every test run.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_da4b8397358543c6b3a5a0e7e5e731e2~mv2.png"
-            alt="Resources Availability Status in Real-time"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_da4b8397358543c6b3a5a0e7e5e731e2~mv2.png"
+              alt="Resources Availability Status in Real-time"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem>
         <svelte:fragment slot="title">Process Standardization</svelte:fragment>
         <article>
           <p>
-            Track the work order process, use various work orders associated with your assets, test articles, build, and report progress to requesters, so they know the value you produce.
+            Track the work order process, use various work orders associated
+            with your assets, test articles, build, and report progress to
+            requesters, so they know the value you produce.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_52ed51684fba42be93fab4418ba9234e~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_52ed51684fba42be93fab4418ba9234e~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
       <AccordianItem>
         <svelte:fragment slot="title">Test Management</svelte:fragment>
         <article>
           <p>
-            Automatically manage your test lab, conduct equipment inventory, manage preventive maintenance schedule and derive insights into a lab's performance to boost productivity and update the team on test lab specifications. 
+            Automatically manage your test lab, conduct equipment inventory,
+            manage preventive maintenance schedule and derive insights into a
+            lab's performance to boost productivity and update the team on test
+            lab specifications.
           </p>
-          <img
-            transition:fade={{duration:800}}
-            src="https://static.wixstatic.com/media/43794f_8ecfb0a9946843ff9925fbb43f3df792~mv2.png"
-            alt="Resource Assignment"
-          />
+          <div class="image-container" transition:fade={{ duration: 800 }}>
+            <img
+              src="https://static.wixstatic.com/media/43794f_8ecfb0a9946843ff9925fbb43f3df792~mv2.png"
+              alt="Resource Assignment"
+            />
+          </div>
         </article>
       </AccordianItem>
     </Accordian>
   </div>
 </section>
+
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap");
 
@@ -180,23 +238,25 @@
     box-sizing: border-box;
   }
 
-
-
+  :global(body) {
+    margin: 0;
+    padding: 2em;
+  }
   h1 {
     max-width: 550px;
     font-size: 32px;
     font-family: "Montserrat", sans-serif;
     font-weight: 800;
     color: #253858;
-    padding: 0 .5em;
+    padding: 0 0.5em;
   }
 
-  h1 span{
-    color: #008AFC;
+  h1 span {
+    color: #008afc;
   }
   .accordian {
     width: 45vw;
-  
+
     color: #253858;
     font-size: 14px;
   }
@@ -206,17 +266,21 @@
     min-height: 550px;
     display: flex;
     flex-direction: column;
-    gap:25px;
+    gap: 25px;
     margin-bottom: 5em;
   }
 
-  .right-align{
+  .right-align {
     align-items: flex-end;
     text-align: right;
   }
 
-  .right-align img{
+  .right-align .image-container {
     left: 0;
+  }
+
+  .right-align h1 span {
+    color: #8d7ad3;
   }
 
   p {
@@ -226,14 +290,82 @@
     margin: 0;
     letter-spacing: 1px;
   }
-  img {
+
+  .image-container {
     width: 45vw;
     top: 0;
     height: 100%;
     right: 0;
-    object-fit: contain;
     position: absolute;
-    
+    margin: 2em;
+  }
+
+  img {
+    object-fit: contain;
+    margin-top: 4em;
+    width: inherit;
+    position: relative;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    border-radius: 20px;
+  }
+  .image-container::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 20%;
+    aspect-ratio: 16/14;
+    background-color: #008afc;
+    border-radius: 20px;
+    opacity: 0.2;
+    animation: breathe 7s linear infinite;
+  }
+
+  .right-align .image-container::after {
+    background-color: #8d7ad3;
+  }
+
+  .right-align .image-container::before {
+    background-color: #8d7ad3;
+  }
+
+  .image-container::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: var(--image-height);
+    width: 13%;
+    aspect-ratio: 1/1;
+
+    background-color: #008afc;
+    border-radius: 20px;
+    opacity: 0.2;
+    z-index: -1;
+    animation: breathe2 4s ease-in infinite;
+  }
+
+  @keyframes breathe {
+    0% {
+      transform: translate(40px, 30px);
+    }
+    50% {
+      transform: translate(15px, 40px);
+    }
+    100% {
+      transform: translate(40px, 30px);
+    }
+  }
+
+  @keyframes breathe2 {
+    0% {
+      transform: translate(-20px, -10px);
+    }
+    50% {
+      transform: translate(-20px, 10px);
+    }
+    100% {
+      transform: translate(-20px, -10px);
+    }
   }
 
   article {
@@ -243,14 +375,72 @@
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
   }
-  @media screen and (max-width: 768px) {
+  @media screen and (max-width: 1100px) {
     .accordian {
       width: 100%;
-      text-align: center;
+      font-size: 14px;
     }
-    img {
-      width: 90%;
-      position: static;
+
+    .accordian-container {
+    position: relative;
+    min-height: 550px;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    margin-bottom: 2em;
+  }
+
+    .image-container {
+      width: 60vw;
+
+      position: relative;
+      margin: 0 auto;
+    }
+
+    h1 {
+      font-size: 20px;
+    }
+    article {
+      padding: 2em 3em;
+      padding-top: 0;
+    }
+
+   img{
+    border-radius: 10px;
+   }
+    .image-container::after {
+      top: auto;
+      bottom: 0;
+      border-radius: 10px;
+    }
+
+    .image-container::before {
+      border-radius: 10px;
+    }
+
+    .right-align {
+   
+    text-align: left;
+  }
+
+    @keyframes breathe2 {
+      0% {
+        transform: translate(-20px, 10px);
+      }
+      50% {
+        transform: translate(-20px, 20px);
+      }
+      100% {
+        transform: translate(-20px, 10px);
+      }
+    }
+  }
+
+
+  @media screen and (max-width: 500px) {
+    .accordian {
+      width: 100%;
+      font-size: 12px;
     }
   }
 </style>
--- src/lib/AccordianItem.svelte
+++ src/lib/AccordianItem.svelte
@@ -18,32 +18,32 @@
   if (items.length === 1) {
     selectItem(id);
   }
-
-  
 </script>
 
-<button on:click={toggle} class:selected={$openItem === id} class:align-right={alignRight} >
+<button
+  on:click={toggle}
+  class:selected={$openItem === id}
+  class:align-right={alignRight}
+>
   {#if $openItem === id}
-    
-      <svg viewBox="0 0 24 24" width="24" height="24" fill="#4ea8de">
-        <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path>
-      </svg>
-    
+    <svg viewBox="0 0 24 24" width="24" height="24" fill="#4ea8de">
+      <path
+        d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"
+      ></path>
+    </svg>
   {:else}
-   
-      <svg viewBox="0 0 24 24" width="24" height="24" >
-        <path
-          d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"
-        ></path>
-      </svg>
-    
+    <svg viewBox="0 0 24 24" width="24" height="24">
+      <path
+        d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"
+      ></path>
+    </svg>
   {/if}
 
   <slot name="title" />
 </button>
 
 {#if $openItem === id}
-  <section   in:slide={{duration:200}} out:slide={{duration:200}}>
+  <section in:slide={{ duration: 200 }} out:slide={{ duration: 200 }}>
     <slot />
   </section>
 {/if}
@@ -51,12 +51,12 @@
 <style>
   button {
     width: 100%;
-    
+
     font-size: 18px;
     padding: 1em;
     background: transparent;
     font-weight: 500;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     border-radius: 0;
     cursor: pointer;
     outline: 0;
@@ -65,22 +65,34 @@
     border-top-left-radius: 20px;
     transition: background-color 200ms ease-in-out;
     display: flex;
-    
-    gap:10px;
-    letter-spacing: .8px;
+
+    gap: 10px;
+    letter-spacing: 0.8px;
     border: 0;
-    
   }
 
-  .align-right{
+  .align-right {
     justify-content: flex-end;
   }
 
   .selected {
     border-color: transparent;
     background-color: rgba(220, 220, 220, 0.175);
-    color: #008AFC;
+    color: #008afc;
     font-weight: 600;
-    
+  }
+
+  .align-right.selected {
+    color: #8d7ad3;
+  }
+
+  @media screen and (max-width: 1100px) {
+    .align-right {
+      justify-content: flex-start;
+    }
+
+    button{
+      font-size: 14px;
+    }
   }
 </style>

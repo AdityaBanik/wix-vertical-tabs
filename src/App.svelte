19b5db1d4441fbd35e52eb960145a2492edcfa1d
Aditya <aditya.banik@12thwonder.com>
2024-01-09 13:37:20 +0530

    final commit

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import { onDestroy, onMount } from "svelte";
   import Accordian from "./lib/Accordian.svelte";
   import AccordianItem from "./lib/AccordianItem.svelte";
+  import data from "./lib/data.js";
 
   let sections = [];
   let selected = 0;
@@ -47,347 +48,42 @@
   <div class="scroll-container">
     <div class="features">
       <h3>Jump to Section</h3>
-      <button class:selected={selected === 0} on:click={() => scrollIntoView(0)}
-        >Schedule of Test Labs</button
-      >
-      <button class:selected={selected === 1} on:click={() => scrollIntoView(1)}
-        >Add or Modify Test Lab</button
-      >
-      <button class:selected={selected === 2} on:click={() => scrollIntoView(2)}
-        >Schedule Maintenance & Calibration</button
-      >
-      <button class:selected={selected === 3} on:click={() => scrollIntoView(3)}
-        >Configure Lab Timings</button
-      >
-      <button class:selected={selected === 4} on:click={() => scrollIntoView(4)}
-        >Define Roles and Assign Users</button
-      >
-      <button class:selected={selected === 5} on:click={() => scrollIntoView(5)}
-        >Configure Capacity & Capabilities</button
-      >
-      <button class:selected={selected === 6} on:click={() => scrollIntoView(6)}
-        >Maintenance and Calibration Records</button
-      >
-      <button class:selected={selected === 7} on:click={() => scrollIntoView(7)}
-        >Conflict Notification</button
-      >
-      <button class:selected={selected === 8} on:click={() => scrollIntoView(8)}
-        >Smart Permissions</button
-      >
-      <button class:selected={selected === 9} on:click={() => scrollIntoView(9)}
-        >Watch Live</button
-      >
-      <button class:selected={selected === 10} on:click={() => scrollIntoView(10)}
-        >More Features</button
-      >
+      {#each data as item, i}
+        <button
+          class:selected={selected === i}
+          on:click={() => scrollIntoView(i)}
+          >{item.title}
+        </button>
+      {/each}
     </div>
     <section class="scrollable">
-      <article
-        class="panel"
-        class:selected={selected === 0}
-        bind:this={sections[0]}
-      >
-        <p>
-          Access a consolidated view of all scheduled events on the test
-          facilities. Effortlessly observe the details of each reservation and
-          reserve labs for events, including test requests
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_047eed2219aa48d3ac9d4fba777bef35~mv2.png"
-          alt="Create Issues from Test"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 1}
-        bind:this={sections[1]}
-      >
-        <p>
-          Create test labs and adjust your lab settings to accommodate evolving
-          requirements. Manage supplier information, including their contact
-          details, for seamless coordination.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_2fe7c38b412e4f908227f50809a3e8da~mv2.png"
-          alt="Issue dashboard"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 2}
-        bind:this={sections[2]}
-      >
-        <p>
-          Sync schedules seamlessly, from recurring calibrations for precision to
-          timely maintenance alerts, ensuring your lab operates at peak
-          reliability and performance.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_2873971af3ff436199584bee57918492~mv2.png"
-          alt="Categorize Issues"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 3}
-        bind:this={sections[3]}
-      >
-        <p>
-          Define operational hours for your lab, streamlining test execution
-          according to your preferred timings and maximize resource efficiency by
-          aligning facility hours with your needs.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_cc6ed2f42bf149aba67a7d322abfa288~mv2.png"
-          alt="Stay informed"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 4}
-        bind:this={sections[4]}
-      >
-        <p>
-          Include users for defined roles to streamline responsibilities.
-          Efficiently delegate tasks for a well-oiled testing process.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_a3dcbcda344c46098cf8d9ceed1c094a~mv2.png"
-          alt="Capture Root Cause and Add Countermeasures"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 5}
-        bind:this={sections[5]}
-      >
-        <p>
-          Limit the concurrent tests in each lab to avoid conflicts and maintain
-          peak capacity. Map tests to specific labs for streamlined planning and
-          efficient resource usage.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_1ccfb3a3cc9e490eae5a27871851aae4~mv2.png"
-          alt="Prioritize Issues"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 6}
-        bind:this={sections[6]}
-      >
-        <p>
-          Access comprehensive records for maintenance and calibration tasks,
-          keeping a complete history. Easily track records for audits and quality
-          assurance.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_cdb8c622c4be4bacbf3bb96ac97f09ce~mv2.png"
-          alt="Link Issues"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 7}
-        bind:this={sections[7]}
-      >
-        <p>
-          Prevent overbooking with intelligent conflict detection, eliminating
-          double-booked resources assisting in smooth operations with no
-          unnecessary interruptions.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_79b9e21a78e14b92af80c49607e05dc5~mv2.png"
-          alt="Watch Live"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 8}
-        bind:this={sections[8]}
-      >
-        <p>
-          Tailor user permissions to ensure security and data integrity and
-          safeguard your lab’s sensitive information with precise user and
-          multiple team access.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_d869aa76548c4adeb21d320aa3969875~mv2.png"
-          alt="Issue Statistics"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 9}
-        bind:this={sections[9]}
-      >
-        <p>
-          Tune in live to monitor test executions, making critical decisions in
-          real-time and ensure quality and efficiency as you virtually oversee
-          testing labs.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_a0d4c61e861d4438be98ce5a470215d9~mv2.png"
-          alt="User Time Log"
-        />
-      </article>
-  
-      <article
-        class="panel"
-        class:selected={selected === 10}
-        bind:this={sections[10]}
-      >
-        <p>More features include</p>
-        <img
-          src="https://static.wixstatic.com/media/b8fe2a_c9114b3c038142c78d0a2f2ebf01a4ea~mv2.png"
-          alt="More Features"
-        />
-      </article>
+      {#each data as item, i}
+        <article
+          class="panel"
+          class:selected={selected === i}
+          bind:this={sections[i]}
+        >
+          <p>{item.description}</p>
+          <img src={item.image} alt={item.title} />
+        </article>
+      {/each}
     </section>
   </div>
-  
 
   <section class="accordian">
     <Accordian>
-      <AccordianItem>
-        <svelte:fragment slot="title">Schedule of Test Labs</svelte:fragment>
-        <p>
-          Access a consolidated view of all scheduled events on the test facilities. Effortlessly observe the details of each reservation and reserve labs for events, including test requests.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_047eed2219aa48d3ac9d4fba777bef35~mv2.png"
-          alt="Schedule of Test Labs"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title">Add or Modify Test Lab</svelte:fragment>
-        <p>
-          Create test labs and adjust your lab settings to accommodate evolving
-          requirements. Manage supplier information, including their contact
-          details, for seamless coordination.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_2fe7c38b412e4f908227f50809a3e8da~mv2.png"
-          alt="Add or Modify Test Lab"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title"
-          >Schedule Maintenance & Calibration</svelte:fragment
-        >
-        <p>
-          Sync schedules seamlessly, from recurring calibrations for precision
-          to timely maintenance alerts, ensuring your lab operates at peak
-          reliability and performance.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_2873971af3ff436199584bee57918492~mv2.png"
-          alt="Schedule Maintenance & Calibration"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title">Configure Lab Timings</svelte:fragment>
-        <p>
-          Define operational hours for your lab, streamlining test execution
-          according to your preferred timings and maximize resource efficiency
-          by aligning facility hours with your needs.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_cc6ed2f42bf149aba67a7d322abfa288~mv2.png"
-          alt="Configure Lab Timings"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title"
-          >Define Roles and Assign Users</svelte:fragment
-        >
-        <p>
-          Include users for defined roles to streamline responsibilities. Efficiently delegate tasks for a well-oiled testing process.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_a3dcbcda344c46098cf8d9ceed1c094a~mv2.png"
-          alt="Define Roles and Assign Users"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title"
-          >Configure Capacity & Capabilities</svelte:fragment
-        >
-        <p>
-          Set limits on the number of tests that can run simultaneously in your lab, preventing conflicts and ensuring your lab operates at peak capacity without overloading. Additionally, leverage
-          the capability to map tests to specific labs for streamlined
-          planning and optimal resource utilization.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_1ccfb3a3cc9e490eae5a27871851aae4~mv2.png"
-          alt="Configure Capacity & Capabilities"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title"
-          >Maintenance and Calibration Records</svelte:fragment
-        >
-        <p>
-          Access comprehensive records for maintenance and calibration tasks, keeping a complete history. Easily track records for audits
-          and quality assurance.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_cdb8c622c4be4bacbf3bb96ac97f09ce~mv2.png"
-          alt="Maintenance and Calibration Records"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title">Conflict Notification</svelte:fragment>
-        <p>
-          Prevent overbooking with intelligent conflict detection, eliminating double-booked resources assisting in smooth operations with no unnecessary interruptions.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_a0d4c61e861d4438be98ce5a470215d9~mv2.png"
-          alt="Conflict Notification"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title">Smart Permissions</svelte:fragment>
-        <p>
-          Tailor user permissions to ensure security and data integrity and safeguard your lab’s sensitive information with precise user and multiple team access.
-        </p>
-        <img
-          src="https://static.wixstatic.com/media/454d4b_d869aa76548c4adeb21d320aa3969875~mv2.png"
-          alt="Smart Permissions"
-        />
-      </AccordianItem>
-
-      <AccordianItem>
-        <svelte:fragment slot="title">More Features</svelte:fragment>
-        <p>More features include</p>
-        <img
-          src="https://static.wixstatic.com/media/b8fe2a_18096b6ad804432ba47aeb5ef21b94c7~mv2.png"
-          alt="More Features"
-        />
-      </AccordianItem>
+      {#each data as item, i}
+        <AccordianItem>
+          <svelte:fragment slot="title">{item.title}</svelte:fragment>
+          <p>{item.description}</p>
+          <img src={item.image} alt={item.title} />
+        </AccordianItem>
+      {/each}
     </Accordian>
   </section>
 </div>
 
 <style>
- 
   ::-webkit-scrollbar {
     width: 6px; /* Set the width of the scrollbar */
   }
@@ -412,12 +108,11 @@
 
     padding: 2.5em 1em;
     display: flex;
-   
+
     flex-direction: column-reverse;
   }
 
   .panel p {
-    
     color: #333333d0;
     margin-top: 3em;
   }
@@ -444,10 +139,10 @@
     transition: color 0.5s ease-in-out;
   }
 
-  .features h3{
+  .features h3 {
     text-transform: uppercase;
     margin-bottom: 2.3em;
-   
+
     color: #717171;
   }
 
@@ -474,7 +169,6 @@
   .container {
     font-family: "Montserrat", sans-serif;
     background: white;
-    
   }
   * {
     box-sizing: border-box;
@@ -482,10 +176,9 @@
 
   .container {
     font-size: 16px;
-   
   }
 
-  .scroll-container{
+  .scroll-container {
     display: flex;
     gap: 120px;
     padding: 4em 2em;
@@ -534,7 +227,7 @@
       font-size: 20px;
     }
 
-    .panel p{
+    .panel p {
       font-size: 18px;
     }
   }
@@ -544,7 +237,7 @@
       font-size: 14px; /* Decrease the font size for mobile */
     }
 
-    .scrollable{
+    .scrollable {
       height: 400px;
     }
     .features button {
---
+++ src/lib/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        title: 'Resources Availability Status',
+        description: 'Explore the calendar view to track the schedule of resources, encompassing their current status, upcoming assignments, and their availability.',
+        image: 'https://static.wixstatic.com/media/454d4b_818cb260ad9244f3b17c54b4b07d8fc1~mv2.png',
+    },
+    {
+        title: 'Effortless Equipment Tracking',
+        description: 'When checking out/checking in an equipment, record the equipment\'s location through a simple check-in and checkout procedure, ensuring nothing gets misplaced.',
+        image: 'https://static.wixstatic.com/media/b8fe2a_e8f5e4ac47494febbd0f396c2b906a83~mv2.png',
+    },
+    {
+        title: 'Equipment Reservation Calendar',
+        description: 'Schedule and reserve equipment in a shared calendar for organized allocation and planning. You can schedule it for test requests/events.',
+        image: 'https://static.wixstatic.com/media/454d4b_66eda8b1a25e46d9bee1622d73a6f0bd~mv2.png',
+    },
+    {
+        title: 'Work Order Scheduling',
+        description: 'Plan ad hoc maintenance or calibration schedules in advance and receive timely notifications, preventing unexpected breakdowns and reducing downtime.',
+        image: 'https://static.wixstatic.com/media/b8fe2a_0fb5f4c16d6b4677ae3030308aa880d3~mv2.png',
+    },
+    {
+        title: 'Preventive Maintenance Logs',
+        description: 'Reminder for the upcoming calibration events and keeping all records of calibration/maintenance activities.',
+        image: 'https://static.wixstatic.com/media/454d4b_e389328ab4654687847ef5ba64e4e3b1~mv2.png',
+    },
+    {
+        title: 'Utilization Record',
+        description: 'Easily assists Overall Equipment Effectiveness (OEE) and track usage records, empowering you to optimize and forecast your asset performance.',
+        image: 'https://static.wixstatic.com/media/454d4b_7e85dafae2004557b0a4ba159035b9a6~mv2.png',
+    },
+    {
+        title: 'Equipment Details Management',
+        description: 'Centralize and update equipment data, including name, type, specs, purchase details, manufacturer, supplier, and warranty information for efficient management.',
+        image: 'https://static.wixstatic.com/media/454d4b_6890023ebab84434bbb287d426bb8c6e~mv2.png',
+    },
+    {
+        title: 'Clone and Archive Equipment',
+        description: 'Effortlessly duplicate equipment profiles, simplify additions, and streamline retirement with preserved records and archives for efficient lifecycle management.',
+        image: 'https://static.wixstatic.com/media/454d4b_784158ea37b84b37b16b81fe5dcc710a~mv2.png',
+    },
+    {
+        title: 'Overdue Checked-Out Equipment',
+        description: 'Receive alerts for overdue equipment checkouts, ensuring equipment is returned on time and avoiding unnecessary delays.',
+        image: 'https://static.wixstatic.com/media/454d4b_9e1a320933fc4c498aecfe79fbaa6d17~mv2.png',
+    },
+    {
+        title: 'Barcode Printing for Identification',
+        description: 'Generate unique barcodes for each piece of equipment, simplifying identification and tracking of warranty, calibration, and other essential details.',
+        image: 'https://static.wixstatic.com/media/454d4b_4e5677fce5484cbcb5870884e351e219~mv2.png',
+    },
+    {
+        title: 'Other Features',
+        description: 'Other features include',
+        image: 'https://static.wixstatic.com/media/b8fe2a_c9114b3c038142c78d0a2f2ebf01a4ea~mv2.png',
+    },
+];
+
+export default data;
